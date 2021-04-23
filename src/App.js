384e9a800937b319fa1bff17c265d30d69770c27
Bruno Brito <brunovinicius31121995@gmail.com>
2021-04-22 23:19:10 -0300

    bugfix

--- src/App.js
+++ src/App.js
@@ -6,12 +6,38 @@ import PacoteLivros from "./pages/Pacote-Livros-Caio-Bottura";
 import JaTraining from "./pages/JaTraining";
 import JacintoMoney from "./pages/JacintoMoney";
 import DesenhandoSemDom from "./pages/Desenhando-sem-dom";
+import ScrollToTop from "./components/scrollToTop";
+import { BrowserRouter, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <React.StrictMode>
-      <Home />
-      <Footer name="infoprodutomania.com.br" />
+      <BrowserRouter>
+        <ScrollToTop />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route
+            path="/metodo-violao-8-semanas"
+            exact
+            component={ViolaoEm8Semanas}
+          />
+          <Route
+            path="/pacote-livros-caio-bottura"
+            exact
+            component={PacoteLivros}
+          />
+          <Route path="/ja-training" exact component={JaTraining} />
+          <Route path="/jacinto-money" exact component={JacintoMoney} />
+          <Route
+            path="/desenhando-sem-dom"
+            exact
+            component={DesenhandoSemDom}
+          />
+          <Route component={Home} />
+        </Switch>
+        <Footer name="infoprodutomania.com.br" />
+      </BrowserRouter>
     </React.StrictMode>
   );
 }
--- src/components/home/infoprodutos.js
+++ src/components/home/infoprodutos.js
@@ -1,8 +1,5 @@
+import { Link } from "react-router-dom";
 function createPricing() {
-  const myStyle = {
-    display: "block",
-  };
-
   return (
     <section className="pricing pricing-1 bg-gray" id="pricing">
       <div className="container">
@@ -30,12 +27,14 @@ function createPricing() {
                 <div className="card-img-top"></div>
                 <div className="card-body card-body--distancer">
                   <h5 className="card-title">MV8S</h5>
-                  <a
+                  <Link
+                    to={{
+                      pathname: "/metodo-violao-8-semanas",
+                    }}
                     className="btn btn--marine"
-                    href="https://violao-em-8-semanas.infoprodutomania.com.br/"
                   >
                     Confira já
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -50,12 +49,14 @@ function createPricing() {
                 <div className="card-img-top"></div>
                 <div className="card-body card-body--distancer">
                   <h5 className="card-title">Pacote Caio Bottura</h5>
-                  <a
+                  <Link
+                    to={{
+                      pathname: "/pacote-livros-caio-bottura",
+                    }}
                     className="btn btn--marine"
-                    href="https://livros-digitais-caio-bottura.infoprodutomania.com.br/"
                   >
                     Confira já
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -70,19 +71,21 @@ function createPricing() {
                 <div className="card-img-top"></div>
                 <div className="card-body card-body--distancer">
                   <h5 className="card-title">JA Training</h5>
-                  <a
+                  <Link
+                    to={{
+                      pathname: "/ja-training",
+                    }}
                     className="btn btn--marine"
-                    href="https://ja-training.infoprodutomania.com.br/"
                   >
                     Confira já
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="row mt-20 mb-20">
+        <div className="row mt-20">
           <div
             className=" col-12 col-md-4 col-lg-4 price-table wow fadeInUp"
             data-wow-duration="1s"
@@ -92,12 +95,14 @@ function createPricing() {
                 <div className="card-img-top"></div>
                 <div className="card-body card-body--distancer">
                   <h5 className="card-title">Jacinto Money</h5>
-                  <a
+                  <Link
+                    to={{
+                      pathname: "/jacinto-money",
+                    }}
                     className="btn btn--marine"
-                    href="https://jacinto-money.infoprodutomania.com.br/"
                   >
                     Confira já
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
@@ -112,33 +117,17 @@ function createPricing() {
                 <div className="card-img-top"></div>
                 <div className="card-body card-body--distancer">
                   <h5 className="card-title">Desenhando sem Dom</h5>
-                  <a
+                  <Link
+                    to={{
+                      pathname: "/desenhando-sem-dom",
+                    }}
                     className="btn btn--marine"
-                    href="https://desenhando-sem-dom.infoprodutomania.com.br/"
                   >
                     Confira já
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="row">
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5540347917675823"
-            crossorigin="anonymous"
-          ></script>
-          <div className="col-12 text-center centralize-text">
-            <ins
-              class="adsbygoogle"
-              style={myStyle}
-              data-ad-client="ca-pub-5540347917675823"
-              data-ad-slot="9248653453"
-              data-ad-format="auto"
-              data-full-width-responsive="true"
-            ></ins>
-            <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
           </div>
         </div>
       </div>
